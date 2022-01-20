5c1aca680c303eedcf18124619fe2ce4979a42f5
Jan Kopal <joedmi189@gmail.com>
2022-01-20 19:24:18 +0100

    Extention metody

---
+++ Extentions/Extentions.cs
@@ -0,0 +1,31 @@
+﻿namespace Extentions
+{
+    public static class Extentions
+    {
+        public static bool IsEven(this int num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static bool IsEven(this double num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static bool IsPrime()
+        { 
+            // udelejte sami
+            return true;
+        }
+
+        public static bool IsGreaterThan(this int num, int a)
+        {
+            return num > a;
+        }
+
+        public static int WordCount(this string str)
+        {
+            return str.Split(' ').Length;
+        }
+    }
+}
---
+++ Extentions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Extentions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 16;
+            double myDouble = 3.14;
+
+            Console.WriteLine(Extentions.IsEven(16));
+            Console.WriteLine(myNumber.IsEven());
+            Console.WriteLine(myDouble.IsEven());
+
+            // mame string, ktery ma vice slov a chceme extention
+            // na pocet slov
+            var sentence = "Hello world, how are you?";
+
+            Console.WriteLine(sentence.WordCount());
+
+            var arr = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(myNumber.IsGreaterThan(17));
+        }
+    }
+}
